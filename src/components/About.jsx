c8e503e8d85454b13ebc4b61fece6cace39910a9
UlyssesCortes <ulyssescortes13@gmail.com>
2023-03-04 22:51:54 -0600

    About section compleated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
+
 import { styles } from "../styles";
 import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
@@ -40,20 +42,25 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
+
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3x1 leading-[30px]"
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with eperience in JavaScript and experise in frameworks like React, Node.js and Three.js. I'm a quick learner and collaborate closely with clients to create efficiant, scalablem and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
-      <div className="mt-20 flex flex-wrap gap-10">
+
+      <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
-
       </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default SectionWrapper(About, "about");
