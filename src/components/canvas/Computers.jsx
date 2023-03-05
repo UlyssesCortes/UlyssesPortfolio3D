9ecd5e38efed01c0080808eade33d1f0ba892703
UlyssesCortes <ulyssescortes13@gmail.com>
2023-03-04 21:11:01 -0600

    Ligning and positioning of model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,10 +7,25 @@ const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
-      <hemisphereLight intensity={0.15}
+      <hemisphereLight
+        intensity={0.15}
         groundColor="black" />
-      <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <pointLight
+        intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
 }
