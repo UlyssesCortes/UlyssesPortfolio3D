ea9f9e0daf1080948c44aea043c1451b06da9616
UlyssesCortes <ulyssescortes13@gmail.com>
2023-03-04 20:44:03 -0600

    Nav Bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Works, StarsCanvas, Tech } from './components'
 
 const App = () => {
 
   return (
-    <div>3D Developer Portfolio</div>
+    <BrowserRouter>
+      <div className="relative z-0 big-primary">
+        <div className="bg-hero-pattern br-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
