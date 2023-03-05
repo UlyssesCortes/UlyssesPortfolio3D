b617177a02bce2c5edf8b1ca0812a0c1e51cadfe
UlyssesCortes <ulyssescortes13@gmail.com>
2023-03-04 21:14:29 -0600

    Loading screen when reloading

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei"
 
 const Loader = () => {
+  const { progress } = useProgress();
   return (
-    <div>Loader</div>
+    <Html>
+      <span className="canvs-load"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: '#f1f1f1',
+          fontWeight: 800,
+          marginTop: 40
+        }}
+      >{progress.toFixed(2)}%</p>
+    </Html>
   )
 }
 
