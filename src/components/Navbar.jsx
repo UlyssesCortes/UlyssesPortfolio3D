03a10ba9198e7fd9eb05a9ecd523382957870270
UlyssesCortes <ulyssescortes13@gmail.com>
2023-03-15 14:08:01 -0600

    scroll fixed for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,22 +7,7 @@ import { menu, close } from '../assets';
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
-      if (scrollTop > 100) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
   return (
     <nav
       className={`${styles.paddingX
