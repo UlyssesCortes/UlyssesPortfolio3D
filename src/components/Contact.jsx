20211c6f4d437dac2caeb9ba2eef52aede4346eb
UlyssesCortes <ulyssescortes13@gmail.com>
2023-04-08 18:02:57 -0700

    Contact form error handler initiated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,6 +16,9 @@ const Contact = () => {
   });
 
   const [loading, setLoading] = useState(false);
+  const [notName, setNotName] = useState(false)
+  const [notEmail, setNotEmail] = useState(false)
+  const [notMessage, setNotMessage] = useState(false)
 
   const handleChange = (e) => {
     const { target } = e;
@@ -26,42 +29,57 @@ const Contact = () => {
       [name]: value,
     });
 
+    form.name ? setNotName(false) : setNotName(true)
+    form.email.includes("@") ? setNotEmail(false) : setNotEmail(true)
+    form.message ? setNotMessage(false) : setNotMessage(true)
+
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLoading(true);
+    if (form.name === "" || form.email === "" || !form.email.includes("@") || form.message === "") {
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "Ulysses Cortes",
-          from_email: form.email,
-          to_email: "ulyssescortes13@gmail.com",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+      form.name === "" && setNotName(true)
+      form.email === "" && setNotEmail(true)
+      form.message === "" && setNotMessage(true)
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
+    } else {
+
+
+      setLoading(true);
+
+      emailjs
+        .send(
+          import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+          {
+            from_name: form.name,
+            to_name: "Ulysses Cortes",
+            from_email: form.email,
+            to_email: "ulyssescortes13@gmail.com",
+            message: form.message,
+          },
+          import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        )
+        .then(
+          () => {
+            setLoading(false);
+            alert("Thank you. I will get back to you as soon as possible.");
+
+            setForm({
+              name: "",
+              email: "",
+              message: "",
+            });
+          },
+          (error) => {
+            setLoading(false);
+            console.error(error);
+            alert("Ahh, something went wrong. Please try again.");
+          }
+        );
+    }
+
   };
 
   return (
@@ -81,29 +99,33 @@ const Contact = () => {
           className='mt-12 flex flex-col gap-8'
         >
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+            <span className='text-white font-medium mb-4'>Name</span>
             <input
               type='text'
               name='name'
               value={form.name}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="What's your name?"
               className='py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
+            {form.name === "" && notName && <p className="formError">Name is required.</p>}
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
+            <span className='text-white font-medium mb-4'>Email</span>
             <input
               type='email'
               name='email'
               value={form.email}
               onChange={handleChange}
-              placeholder="What's your web address?"
+              placeholder="What's your email address?"
               className='py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
+
+            {!form.email.includes("@") && notEmail && <p className="formError">Please enter valid email adress.</p>}
+
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
+            <span className='text-white font-medium mb-4'>Message</span>
             <textarea
               rows={7}
               name='message'
@@ -112,6 +134,8 @@ const Contact = () => {
               placeholder='What you want to say?'
               className='py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
+            {form.message === "" && notMessage && <p className="formError">Message is required.</p>}
+
           </label>
 
           <button
