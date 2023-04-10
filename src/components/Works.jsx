31cac13f5406956f7886eac72671e8d0f74aa773
UlyssesCortes <ulyssescortes13@gmail.com>
2023-04-10 00:43:48 -0700

    git link fixed

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -48,20 +48,15 @@ const ProjectCard = ({
               alt='project_image'
               className='projImg'
             />
-
-            <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-              <div
-                onClick={() => window.open(source_code_link, "_blank")}
-                className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer gitLinkSource'
-              >
-                <img
-                  src={github}
-                  alt='source code'
-                  className='w-1/2 h-1/2 object-contain '
-                />
-              </div>
-            </div>
           </div>
+
+          <img
+            src={github}
+            alt='githubCode'
+            className='gitLinkSource'
+            onClick={() => window.open(source_code_link, "_blank")}
+
+          />
         </section>
 
         <div className='descriptionContainer'>
